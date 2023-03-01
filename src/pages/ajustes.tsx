d48fd7e14248930f645f7a7b0d5aed2c6fbeea31
matheuswisch <wisch.pg@outlook.com>
2023-03-01 15:07:30 -0300

    Style and Fix: add logo and adjustments in folders

--- src/pages/ajustes.tsx
+++ src/pages/ajustes.tsx
@@ -1,10 +1,8 @@
 import Layout from "@/components/template/layout"
 
 
-export default function Home() {
-
+export default function Ajustes() {
   return (
-
     <Layout titulo={'Ajustes e Configurações'} subtitulo={'Personalize o sistema por aqui!'}>
       <h3>conteudo</h3>
     </Layout>
